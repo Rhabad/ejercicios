ad8c6473fe0222b2e2b4636a9e01757a14d9471d
rhabad <nicolaspizarrozambra@gmail.com>
2025-03-27 01:21:08 -0300

    funcionalidad a los tab

--- tabs/archivojs.js
+++ tabs/archivojs.js
@@ -1,1 +1,20 @@
 window.onload = inicio;
+
+// -al presionar tab muestra contenido
+// -capturar el click
+// -ocultamos los demas contenido que no queremos
+// -mostrar solo el contenido deseado
+function inicio() {
+  let btns = document.querySelectorAll("button");
+  let tabs = document.querySelectorAll(".tab");
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      btns.forEach((btn) => btn.classList.remove("btn-activo"));
+      tabs.forEach((tab) => tab.classList.remove("tab-active"));
+
+      this.classList.add("btn-active");
+      document.getElementById(this.dataset.tab).classList.add("tab-active");
+    });
+  });
+}
