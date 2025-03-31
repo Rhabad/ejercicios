876ef773fb90532f5e51d21f34cd24be35831589
rhabad <nicolaspizarro.450@gmail.com>
2025-03-31 15:15:06 -0300

    restriccion de caracteres, modificando el textarea

--- restricted-textarea/restricted.js
+++ restricted-textarea/restricted.js
@@ -1,1 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  textAreaCharacter();
+});
+
+const txtArea = document.querySelectorAll(".txtarea");
+const areaContain = document.querySelectorAll(".txtarea-contain");
+const contador = document.querySelectorAll("#count");
+function textAreaCharacter() {
+  txtArea.forEach((text, indice) => {
+    text.addEventListener("input", () => {
+      let maxCarac = 250;
+      if (text.value.length > maxCarac) {
+        text.value = text.value.substring(0, maxCarac);
+        areaContain[indice].style.border = "3px solid red";
+        areaContain[indice].style.color = "red";
+        text.style.color = "red";
+      }
+
+      contador[indice].textContent = `${text.value.length} / ${maxCarac}`;
+    });
+  });
+}
