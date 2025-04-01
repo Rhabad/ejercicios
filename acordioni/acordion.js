273c8350340c9c54d46fc1b2cea71840e0df1cdd
rhabad <nicolaspizarro.450@gmail.com>
2025-03-31 22:57:58 -0300

    Agragando la funcoinalidad al acordion, mostrando contenido

--- acordioni/acordion.js
+++ acordioni/acordion.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  desplegarContenido();
+});
+
+const items = document.querySelectorAll(".item");
+const itemsContenido = document.querySelectorAll(".item__contenido");
+function desplegarContenido() {
+  items.forEach((item, indice) => {
+    item.addEventListener("click", () => {
+      let estiloActual = window.getComputedStyle(
+        itemsContenido[indice]
+      ).display;
+      /* agregamos la clase para que aparezca el contenido */
+      if (estiloActual == "none") {
+        itemsContenido[indice].style.display = "block";
+      } else {
+        itemsContenido[indice].style.display = "none";
+      }
+    });
+  });
+}
