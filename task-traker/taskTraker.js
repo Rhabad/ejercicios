df35dcba15050e3e79f7616e1b775c71fd999e5f
rhabad <nicolaspizarro.450@gmail.com>
2025-04-02 15:18:43 -0300

    agregando tarea, gap en tarea creada

--- task-traker/taskTraker.js
+++ task-traker/taskTraker.js
@@ -1,1 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  crearTarea();
+});
+
+const task = document.querySelector('.create-task input[type="text"]');
+const btnCrearTask = document.querySelector(".create-task button");
+function crearTarea() {
+  task.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      /* console.log("pre"); */
+      btnCrearTask.style.backgroundColor = "#0009";
+      nuevaTarea();
+      setTimeout(() => {
+        btnCrearTask.style.backgroundColor = "#000";
+      }, 200);
+    }
+  });
+
+  btnCrearTask.addEventListener("click", nuevaTarea);
+}
+
+const containTask = document.querySelector(".task-tracker__tasks");
+function nuevaTarea() {
+  let tareaNueva = task.value;
+
+  /* crear el div con la tarea */
+  let formatoTarea = `<div class="tasks">
+                        <input type="checkbox" />
+                        <p>${tareaNueva}</p>
+                        <button>Borrar</button>
+                    </div>`;
+
+  containTask.innerHTML += formatoTarea;
+}
