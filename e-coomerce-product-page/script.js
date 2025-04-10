4791d2623a27a3e4f4dca789141dbb32c99fdf0c
rhabad <nicolaspizarro.450@gmail.com>
2025-04-10 00:47:23 -0400

    funcionalidad al menu desplegable

--- e-coomerce-product-page/script.js
+++ e-coomerce-product-page/script.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  menuDesplegable();
+});
+
+const navItems = document.querySelector("nav");
+const menu = document.querySelector(".logo img");
+function menuDesplegable() {
+  menu.addEventListener("click", () => {
+    let showNav = navItems.classList.contains("mostrar-nav");
+    if (showNav) {
+      navItems.classList.remove("mostrar-nav");
+    } else {
+      navItems.classList.add("mostrar-nav");
+    }
+  });
+}
