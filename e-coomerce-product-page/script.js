0d95d8e2eaab672595b1d38f5e209643fe1eee1e
rhabad <nicolaspizarro.450@gmail.com>
2025-04-10 22:56:14 -0400

    galeria fotos carrusel funcoinalidad

--- e-coomerce-product-page/script.js
+++ e-coomerce-product-page/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   menuDesplegable();
+  cambiarImagen();
 });
 
+/* menu desplegable en dispositivos pequeños */
 const navItems = document.querySelector("nav");
 const menu = document.querySelector(".logo img");
 function menuDesplegable() {
@@ -14,3 +16,34 @@ function menuDesplegable() {
     }
   });
 }
+/* menu desplegable en dispositivos pequeños */
+
+function cambiarImagen() {
+  /* moverse entre las imagenes diferentes */
+  const fotoProducto = document.querySelector(".galeria-foto");
+  const btnIzquierda = document.querySelector(".btn-izquierda");
+  const btnDerecha = document.querySelector(".btn-derecha");
+  let index = 0;
+  let imagenes = [
+    "zapatos/zapatos1.webp",
+    "zapatos/zapato2.jpg",
+    "zapatos/zapato3.jpeg",
+  ];
+
+  btnIzquierda.addEventListener("click", () => {
+    index = (index - 1 + imagenes.length) % imagenes.length;
+    /* console.log(index); */
+    fotoProducto.src = imagenes[index];
+  });
+  btnDerecha.addEventListener("click", () => {
+    index = (index + 1) % imagenes.length;
+    fotoProducto.src = imagenes[index];
+  });
+}
+/* moverse entre las imagenes diferentes */
+
+/* agregar o remover stock */
+/* agregar o remover stock */
+
+/* agregar compra al carrita, solo un mensaje, 
+es para mostrar que se realizo la compra */
