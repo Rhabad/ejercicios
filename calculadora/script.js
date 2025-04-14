02f886b0d658f8fc1c2d42fede358bd47db0edbc
rhabad <nicolaspizarro.450@gmail.com>
2025-04-14 18:00:35 -0400

    calculadora, reset, delet y calcular operacion, tambien se arreglo lo del multiplicar

--- calculadora/script.js
+++ calculadora/script.js
@@ -5,29 +5,49 @@ document.addEventListener("DOMContentLoaded", () => {
 const botones = document.querySelectorAll(".botones__btn-num-oper");
 const reset = document.querySelector(".reset");
 const igual = document.querySelector(".igual");
-let cadenaParaCalcular = "";
+let cadenaParaCalcular = "0";
+let pantalla = document.querySelector(".calculadora-superior__pantalla");
 
 function capturarBtn() {
   botones.forEach((btn) => {
     btn.addEventListener("click", () => {
-      let pantalla = document.querySelector(".calculadora-superior__pantalla");
-
       /* console.log(btn.innerText); */
-      if (pantalla.innerText == "0") {
-        pantalla.innerText = btn.innerText;
-      } else {
-        pantalla.innerText += btn.innerText;
+
+      //aqui borramos un caracter
+      if (btn.innerText == "DEL") {
+        pantalla.innerText = pantalla.innerText.slice(0, -1);
+      } else if (pantalla.innerText.length < 14) {
+        if (pantalla.innerText == "0") {
+          pantalla.innerText = btn.innerText;
+        } else {
+          if (btn.innerText == "x") {
+            pantalla.innerText += "*";
+          } else {
+            pantalla.innerText += btn.innerText;
+          }
+        }
       }
+
       cadenaParaCalcular = pantalla.innerText;
     });
   });
 
+  //presionar el reset, reiniciar calculadora
+  reset.addEventListener("click", () => {
+    pantalla.innerText = "0";
+    cadenaParaCalcular = "0";
+  });
+
   calcular();
 }
-
 function calcular() {
   /* separar la cadena */
   igual.addEventListener("click", () => {
-    console.log(cadenaParaCalcular);
+    /* console.log(cadenaParaCalcular); */
+    if (/^[0-9+\-*/().\s]+$/.test(cadenaParaCalcular)) {
+      pantalla.innerText = eval(cadenaParaCalcular);
+    } else {
+      throw new Error("Operacion no valida");
+    }
   });
 }
